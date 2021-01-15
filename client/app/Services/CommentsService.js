b3c0062c5fd0f9ea836c6cc27d8f913368a9fe5f
ethanvachon <ethanvachon@gmail.com>
2021-01-15 13:30:40 -0700

    services

---
+++ client/app/Services/CommentsService.js
@@ -0,0 +1,15 @@
+import { ProxyState } from "../AppState.js"
+import { api } from "../Services/AxiosService.js"
+import { Comment } from "../Models/Post.js"
+
+class CommentsService {
+  async getPosts() {
+
+  }
+  async addPost(newPost) {
+
+  }
+  async
+}
+
+export const commentsService = new CommentsService()
---
+++ client/app/Services/PostsService.js
@@ -0,0 +1,15 @@
+import { ProxyState } from "../AppState.js"
+import { api } from "../Services/AxiosService.js"
+import { Post } from "../Models/Post.js"
+
+class PostsService {
+  async getPosts() {
+
+  }
+  async addPost(newPost) {
+
+  }
+  async
+}
+
+export const postsService = new PostsService()

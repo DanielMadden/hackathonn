3df1dc53eeae7ea53112bd56871383edbf135a28
Daniel Madden <danielfpjobs@gmail.com>
2021-01-15 19:17:41 -0700

    Updated asynchronous comments, fixed some populations in posts

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose'
 const Schema = mongoose.Schema
+const ObjectId = mongoose.Schema.Types.ObjectId
 
 const Post = new Schema(
   {
     title: { type: String, required: true },
     body: { type: String, required: true },
     votes: { type: Number, required: true },
-    authorId: { type: String, required: true }
+    author: { type: ObjectId, ref: "Author", required: true }
   },
   { timestamps: true, toJSON: { virtuals: true } }
 )
